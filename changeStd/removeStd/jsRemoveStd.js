4a0c4bcd48499ec73bec3a2f8978d37d3f940019
it62070042 <56545080+fchana@users.noreply.github.com>
2021-02-12 00:00:06 +0700

    Add ui

---
+++ changeStd/removeStd/jsRemoveStd.js
@@ -0,0 +1,7 @@
+function showProfile() {
+    check = document.querySelector(".showProfile").style;
+    if (check.display == "block")
+        check.display = "none"
+    else
+        check.display = "block"
+}
